a8d417974b38bcf78dd42ac78e1997c37703ee63
Drew Barontini <drewbarontini@gmail.com>
2015-01-23 09:02:02 -0500

    Add comments to config.rb

--- config.rb
+++ config.rb
@@ -1,22 +1,37 @@
 require 'uglifier'
 
+# ====================================
+#   Activate Plugins
+# ====================================
+
 activate :automatic_image_sizes
 activate :directory_indexes
 activate :livereload
 activate :syntax
 
-set :page_title, 'GIFPoint'
+# ====================================
+#   Global Variables
+# ====================================
 
-ready do
-  sprockets.append_path File.join root, 'bower_components'
-end
+set :page_title, 'GIFPoint'
 
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 
-# Build-specific configuration
+# ====================================
+#   Ready
+# ====================================
+
+ready do
+  sprockets.append_path File.join root, 'bower_components'
+end
+
+# ====================================
+#   Build Configuration
+# ====================================
+
 configure :build do
   activate :minify_css
   activate :minify_javascript, :compressor => ::Uglifier.new(:mangle => false)
